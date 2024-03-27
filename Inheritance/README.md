a90905bf59ce8141f36998f3d45892f9819e9d02
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-03-27 18:49:21 -0400

    Lab 02 "Single Inheritance" from the lesson for Inheritance, a part of Java OOP course. Closes #17

---
+++ Inheritance/README.md
@@ -0,0 +1,4 @@
+Lab 01 - Single Inheritance
+-
+
+Simple example of how one class extends another class and its methods.
---
+++ Inheritance/src/L01SingleInheritance/Dog.java
@@ -0,0 +1,8 @@
+package L01SingleInheritance;
+
+public class Dog extends Animal{
+
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
